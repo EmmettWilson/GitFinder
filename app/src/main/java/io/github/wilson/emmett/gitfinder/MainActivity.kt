40ff4d7c0aa36b9c2cc5a04070f739992caf7903
emmett.wilson <mathematicalfunk@gmail.com>
2019-03-11 18:48:33 -0500

    Migrate to latest production androidx components

--- app/src/main/java/io/github/wilson/emmett/gitfinder/MainActivity.kt
+++ app/src/main/java/io/github/wilson/emmett/gitfinder/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.wilson.emmett.gitfinder
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
